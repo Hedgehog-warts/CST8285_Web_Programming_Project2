d4195fce1045f5872c1c8fdbe408810a9134c1a9
Hedgehog-warts <aldebaran0902@gmail.com>
2022-11-22 03:36:22 -0500

    the recommendation division is partially added

--- js/script.js
+++ js/script.js
@@ -8,6 +8,47 @@ window.addEventListener('scroll', () => {
     }
 });
 
+var vanGridItem = document.querySelectorAll(".vanGridItem");
+var calGridItem = document.querySelectorAll(".calGridItem");
+const allref = [vanGridItem, calGridItem];
+
+var vanLink = document.querySelector("#vancouver");
+var calLink = document.querySelector("#calgary");
+
+vanLink.addEventListener('click', function() {changeRecommend(vanGridItem);});
+calLink.addEventListener('click', function() {changeRecommend(calGridItem);});
+
+function changeRecommend(ref) {
+    for(let i=0; i<allref.length; i++) {
+        for(let j=0; j<ref.length; j++) {
+            allref[i][j].classList.add('visuallyHidden');
+            ref[j].classList.remove('visuallyHidden');
+        }
+    }
+}
+
+
+// function changeRecommend(city, ref) {
+//     switch(city) {
+//         case 'vancouver':
+//             for(let i=0; i<ref.length; i++) {
+//                 console.log(ref);
+//                 calGridItem[i].classList.add('visuallyHidden');
+//                 vanGridItem[i].classList.add('visuallyHidden');
+//                 ref[i].classList.remove('visuallyHidden');
+//             }
+        
+//         break;
+//         case 'calgary':
+//             for(let i=0; i<calGridItem.length; i++) {
+//                 vanGridItem[i].classList.add('visuallyHidden');
+//                 calGridItem[i].classList.remove('visuallyHidden');
+//             }
+
+//         break;
+//     }
+// }
+
 
 var slideIndex = 1;
 var stop = 0;

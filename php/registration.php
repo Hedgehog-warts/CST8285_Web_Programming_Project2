59a0ec24b6a8aff34b6c0f71ac6d5b9a3d5ef077
Hedgehog-warts <aldebaran0902@gmail.com>
2022-11-30 22:22:04 -0500

    sign up php done

---
+++ php/registration.php
@@ -0,0 +1,43 @@
+<form action="../welcome.php" method="post" id="welcome" name="welcome">
+    <input type="text" name="welcomename" value=<?php echo "$_POST[login]";?>>
+</form>
+<script type="text/javascript">
+    document.getElementById('welcome').submit();
+</script>
+<?php  
+
+// database connection code
+if(isset($_POST['email']))
+{
+    // $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
+    $con = mysqli_connect('localhost', 'root', 'abcd1234','webassignment2');
+
+    if(mysqli_connect_errno()) {
+        $msg = "Database connection failed: ";
+        $msg .= mysqli_connect_error();
+        $msg .= " (" . mysqli_connect_errno() . ")";
+        exit($msg);
+    }
+
+    // get the post records
+    $email = $_POST['email'];
+    $userName = $_POST['login'];
+    $password = $_POST['pass'];
+    $province = $_POST['Sort1'];
+    $concept = $_POST['Sort2'];
+
+    // database insert SQL code
+    $sql = "INSERT INTO userinformation (emailAddress, userName, userPassword, preferredProvince, travelConcept)
+            VALUES ('$email', '$userName', '$password', '$province', '$concept')";
+    
+    
+    // insert in database 
+    $rs = mysqli_query($con, $sql);
+    if($rs) {
+        echo "Contact Records Inserted";
+    } else {
+        echo "Are you a genuine visitor?";
+    }
+     
+}
+?>
---
+++ welcome.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo "<div class=\"welcomeDiv\" style=\"margin: auto; width: 50%; padding: 10px; text-align: center\">
+        <h1>$_POST[welcomename], welcome to GitTour!</h1><p>Please log in with your new account</p></div>";
+    ?>
+</body>
+</html>

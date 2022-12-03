956476c32ea717556d7e5baa6867b33320d98b06
Hedgehog-warts <aldebaran0902@gmail.com>
2022-12-02 23:42:45 -0500

    package updated

--- js/package-script.js
+++ js/package-script.js
@@ -5,7 +5,6 @@
 function searchFunction() {
     let searchPackages = document.getElementById('searchBar').value;
     searchPackages = searchPackages.toLowerCase();
-    console.log(searchPackages)
     let packageResults = document.getElementsByClassName("search-packageTitle");
 
     for (i = 0; i < packageResults.length; i++) {
@@ -21,31 +20,28 @@ function searchFunction() {
 
 // Resetting the search bar and the drop down value
 function resetSearch() {
-    document.getElementById('searchBar').value = ''
+    document.getElementById('searchBar').value = '';
     searchFunction();
-    document.getElementById('package-dropDown').value=''
+    document.getElementById('package-dropDown').value='';
 }
-document.getElementById('package-resetButton').addEventListener("click", resetSearch)
-
+document.getElementById('package-resetButton').addEventListener("click", resetSearch);
 
 // Filtering based off the drop down value
 function filterByProvince(selectedProvince) {
     let packageResults = document.getElementsByClassName("search-packageTitle").length;
-    let testing = document.getElementsByClassName("search-packageTitle")
-    let province = document.getElementsByName(selectedProvince)
+    let testing = document.getElementsByClassName("search-packageTitle");
+    let province = document.getElementsByName(selectedProvince);
 
     for (i = 0; i < packageResults; i++) {
-    if (testing[i].innerHTML.includes(selectedProvince)){
-        console.log('finally')
-        testing[i].style.display="list-item"
+        if (testing[i].innerHTML.includes(selectedProvince)){
+
+            testing[i].style.display="list-item"
+        }
+        else{
+            testing[i].style.display="none"
+        }
     }
-    else{
-        testing[i].style.display="none"
-    }
-       
-         
     
-}
 }
 
 var slideIndex = 0;

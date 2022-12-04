c3646e29a1f93440bbc3d31659b68907e9114bbb
Hedgehog-warts <aldebaran0902@gmail.com>
2022-12-03 20:22:25 -0500

    minor updates

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/* Assignement 2 Group 7_Doyoung Kim */
+
 /* Header */
 
 var listLogo = document.querySelector('#hiddenLogo');
@@ -20,30 +22,11 @@ var goTop = document.getElementById("goToTopID");
 window.addEventListener('scroll', () => {
     var scroll = this.scrollY;
     
-
     if (scroll >100 ) {
         goTop.classList.add("newGoToTop");
     } else {
         goTop.classList.remove("newGoToTop");
     }
-
-    // var circle1=document.querySelector('.circle1');
-    // var circle2=document.querySelector('.circle2');
-    // var circle3=document.querySelector('.circle3');
-
-    // circleArray = new Array(circle1, circle2, circle3);
-
-    // for (i=0; i<circleArray.length; i++){
-    //     circleArray[i].classList.remove("newCircle");
-    // }
-
-    // if (scroll > 360 && scroll <1180) {
-    //     circle1.classList.add("newCircle");
-    // } else if (scroll > 1180 && scroll <2180) {
-    //     circle2.classList.add("newCircle");
-    // } else if (scroll > 2180) {
-    //     circle3.classList.add("newCircle");
-    // } 
 });
 
 goTop.addEventListener('click', topFunction);
